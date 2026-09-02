6078ca1ddb54260e314c0cec347f0c7347fa0aa2
Kamal Chauhan <kamalchauhan0037@gmail.com>
2026-09-03 00:26:04 +0530

    ingress

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
    backend "azurerm" {
-    resource_group_name  = "rgvistara"
-    storage_account_name = "stgvistara"
+    resource_group_name  = "rgvistara1"
+    storage_account_name = "stgvistara2"
     container_name       = "kkconvision"
     key                  = "dev.tfstatee"
     
